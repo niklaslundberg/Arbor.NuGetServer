7e59adacf8f5111bf8fa4aaf6095a24bf8b949af
Niklas Lundberg <niklas.lundberg@milou.se>
2016-05-26 10:07:31 +0200

    Updated NuGet.Server package.

--- src/Arbor.NuGetServer.IisHost/DataServices/NuGetRoutes.cs
+++ src/Arbor.NuGetServer.IisHost/DataServices/NuGetRoutes.cs
@@ -5,15 +5,24 @@ using System.Web.Routing;
 using Arbor.KVConfiguration.Core;
 using Arbor.NuGetServer.Core.Extensions;
 
+using NuGet.Server;
 using NuGet.Server.DataServices;
+using NuGet.Server.Publishing;
+
+using RouteMagic;
 
 namespace Arbor.NuGetServer.IisHost.DataServices
 {
     public static class NuGetRoutes
     {
+        private const string OData = "odata";
+
+        private const string ServiceType = "serviceType";
+
         public static void Configure()
         {
             MapRoutes(RouteTable.Routes);
+            ServiceResolver.SetServiceResolver(new DefaultServiceResolver());
         }
 
         private static void MapRoutes(RouteCollection routes)
@@ -24,18 +33,33 @@ namespace Arbor.NuGetServer.IisHost.DataServices
                     $"AppSetting with key '{Key}' is not set");
 
             var factory = new DataServiceHostFactory();
+
+            routes.MapDelegate(
+                "CreatePackageNuGet",
+                nugetRoute,
+                new
+                    {
+                        httpMethod = new HttpMethodConstraint("PUT")
+                    },
+                context => CreatePackageService().CreatePackage(context.HttpContext));
+
             var serviceRoute = new ServiceRoute(nugetRoute, factory, typeof(Packages))
                                    {
                                        Defaults = new RouteValueDictionary
                                                       {
-                                                          { "serviceType", "odata" }
+                                                          { ServiceType, OData }
                                                       },
                                        Constraints = new RouteValueDictionary
                                                          {
-                                                             { "serviceType", "odata" }
+                                                             { ServiceType, OData }
                                                          }
                                    };
             routes.Add(nugetRoute, serviceRoute);
+        }
+
+        private static IPackageService CreatePackageService()
+        {
+            return ServiceResolver.Resolve<IPackageService>();
         }
     }
 }
